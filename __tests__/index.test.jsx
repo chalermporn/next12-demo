eed28909ff938643b3b54b7138a473f3b8bcb9d4
Chalermporn Posoppitakwong <bird.urights@gmail.com>
2021-11-22 22:08:09 +0700

    update jest

--- __tests__/index.test.jsx
+++ __tests__/index.test.jsx
@@ -7,10 +7,16 @@ import { render, screen } from '@testing-library/react'
 import Home from '../pages/index'
 
 describe('Home', () => {
-  it('renders a heading',  async () => {
+  it('renders a heading', () => {
     render(<Home />)
+    const heading = screen.getByRole('heading', {
+      name: /Welcome to Next\.js!/i
+    })
+    expect(heading).toBeInTheDocument()
+  })
 
-    await waitFor(() => screen.getByText('Welcome to Next.js!'))
-    console.log(111,waitFor);
+  it("getbyText Home", () => {
+    const { getByText } = render(<Home />)
+    expect(getByText('Get started by editing')).toBeInTheDocument()
   })
 })
