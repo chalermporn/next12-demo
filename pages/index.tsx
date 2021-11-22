7e50439f68f60ad881c821a59e77e4fb11f0a80b
Chalermporn Posoppitakwong <bird.urights@gmail.com>
2021-11-22 22:52:19 +0700

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
-export async function getServerSideProps(res: any) {
+export async function getServerSideProps({ req, res }) {
   res.setHeader(
     'Cache-Control',
     'public, s-maxage=10, stale-while-revalidate=59'
@@ -15,7 +15,7 @@ export async function getServerSideProps(res: any) {
     },
   }
 }
-const Home: NextPage = (time: any) => {
+const Home: NextPage = ({ time }) => {
   return (
     <div className={styles.container}>
       <Head>
